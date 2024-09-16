8201693b255b063778b1669bb82f590087128d51
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-09-16 20:17:29 -0300

    first commit

---
+++ core/src/test/java/br/com/udemy/backoffice/backofficeIT.java
@@ -0,0 +1,8 @@
+package br.com.udemy.backoffice;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class backofficeIT extends backofficeTest {
+    // Execute the same tests but in packaged mode.
+}
